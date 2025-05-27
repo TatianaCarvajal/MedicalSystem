d09a90efaf9bbae7e320feff7b76e22c7794cfca
Tatiana Carvajal <taticv159912@gmail.com>
2025-05-26 20:20:27 -0500

    First commit

---
+++ src/test/java/com/tatianacarvajal/medicalsystem/MedicalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatianacarvajal.medicalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
