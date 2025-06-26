7357d177c1ae10d1c46f7e0d9a9d50aadd0505b2
Tatiana Carvajal <taticv159912@gmail.com>
2025-06-26 18:00:24 -0500

    Creating update doctor service

---
+++ src/main/java/com/tatianacarvajal/medicalsystem/application/service/UpdateDoctorService.java
@@ -0,0 +1,33 @@
+package com.tatianacarvajal.medicalsystem.application.service;
+
+import com.tatianacarvajal.medicalsystem.domain.entities.Doctor;
+import com.tatianacarvajal.medicalsystem.domain.repository.DoctorRepository;
+import com.tatianacarvajal.medicalsystem.domain.usecases.doctor.UpdateDoctorUseCase;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateDoctorService implements UpdateDoctorUseCase {
+
+    private final DoctorRepository doctorRepository;
+
+    public UpdateDoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    @Override
+    public Doctor update(Doctor doctor) {
+        Long id = doctor.getId();
+        String doctorName = doctor.getName();
+
+        Doctor validatedDoctor = doctorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Doctor was not found with that id: " + id));
+
+        if (doctorName == null || doctorName.isBlank()) {
+           throw new IllegalArgumentException("Doctor name is mandatory");
+        }
+
+        validatedDoctor.setName(doctorName);
+        return doctorRepository.update(validatedDoctor);
+    }
+}
