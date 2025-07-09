7228c2d36907e6f91447b7655eca75a4e3e50fc2
Tatiana Carvajal <taticv159912@gmail.com>
2025-07-09 18:11:53 -0500

    Creating patient entity

---
+++ src/main/java/com/tatianacarvajal/medicalsystem/infrastructure/persistence/entity/PatientEntity.java
@@ -0,0 +1,24 @@
+package com.tatianacarvajal.medicalsystem.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "patients")
+public class PatientEntity {
+
+    @Id
+    @Column(name = "patient_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long patientId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "identification_number")
+    private String document;
+
+    @Column(name = "phone")
+    private String phone;
+}
