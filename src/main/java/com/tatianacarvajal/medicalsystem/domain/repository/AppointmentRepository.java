dbff451407e9aeed56045caccfa8cc3a26afd44e
Tatiana Carvajal <taticv159912@gmail.com>
2025-09-08 15:58:13 -0500

    Adding retrieve logic to appointment repository implementation

--- src/main/java/com/tatianacarvajal/medicalsystem/domain/repository/AppointmentRepository.java
+++ src/main/java/com/tatianacarvajal/medicalsystem/domain/repository/AppointmentRepository.java
@@ -10,7 +10,7 @@ public interface AppointmentRepository {
     Appointment create(Appointment appointment);
     List<Appointment> findAll();
     Optional<Appointment> findById(Long id);
-    List<Appointment> findAllAppointmentsOf(Long patientId);
+    List<Appointment> findAppointmentsByPatientId(Long patientId);
     Optional<Appointment> findByDoctorAvailability(Long doctorId, LocalDateTime dateTime);
     Optional<Appointment> findByPatientAvailability(Long patientId, LocalDateTime dateTime);
     Appointment update(Appointment appointment);
